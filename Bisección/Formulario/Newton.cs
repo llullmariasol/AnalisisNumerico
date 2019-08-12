04656497d850ca29ca50430986a873920d50b0cd
LAPTOP-RIC0EAK5\Carmi <ramirezcarmina1@gmail.com>
2019-08-12 20:33:37 -0300

    Newton
    pull req

---
+++ Bisección/Formulario/Newton.cs
@@ -0,0 +1,50 @@
+﻿using Métodos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Formulario
+{
+    public partial class Newton : Form
+    {
+        public Newton()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        { // double xin, int it, double to, int c, double x, double xrr, double er, double der            
+            double xini = Convert.ToDouble(textBox1.Text);
+            int iter = (int)Convert.ToSingle(textBox3.Text);
+            double tole = Convert.ToSingle(textBox2.Text);
+            Newton_Raphson newton = new Newton_Raphson(xini,iter,tole);
+            Solución solu = newton.Calcular();
+            label10.Text = solu.iteru.ToString();
+            label11.Text =  solu.sol.ToString();
+            label2.Text = Convert.ToDecimal(solu.erel).ToString();
+            label13.Text = solu.Error;
+            if (solu.Error == null)
+            {
+                textBox1.Text = "";
+                textBox2.Text = "";
+                textBox3.Text = "";
+            }
+        }
+
+        private void Newton_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Bisección/Formulario/Unidad1.Designer.cs
+++ Bisección/Formulario/Unidad1.Designer.cs
@@ -88,6 +88,7 @@
             this.button3.TabIndex = 4;
             this.button3.Text = "NEWTON - RAPHSON";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
